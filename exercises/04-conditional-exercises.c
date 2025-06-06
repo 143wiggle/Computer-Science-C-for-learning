ebf956df4ee1567acebba2826e363db493f7e65c
bwent <belmontebrent@gmail.com>
2025-06-06 12:05:06 +0800

    04-conditional-exercises.c

--- exercises/04-conditional-exercises.c
+++ exercises/04-conditional-exercises.c
@@ -1,5 +1,5 @@
 // File: 2c-char-grades-if-else.c
-// Purpose: Takes a letter grade as input and prints feedback using if-else statements.
+// Purpose: Takes a letter grade as input and prints supportive feedback using if-else statements.
 // Student: Brent Frias Belmonte
 
 #include <stdio.h>
@@ -11,22 +11,22 @@ int main() {
     scanf(" %c", &grade); // space before %c handles leftover newline
 
     if (grade == 'A') {
-        printf("Perfect!\n");
+        printf("Perfect! Keep up the amazing work!\n");
     }
     else if (grade == 'B') {
-        printf("You did good!\n");
+        printf("Great job! You're doing really well.\n");
     }
     else if (grade == 'C') {
-        printf("You did okay.\n");
+        printf("Good effort! Keep pushing forward.\n");
     }
     else if (grade == 'D') {
-        printf("At least it's not an F.\n");
+        printf("You're making progress, keep improving!\n");
     }
     else if (grade == 'F') {
-        printf("You failed.\n");
+        printf("Don't worry, every step is a step toward success. Keep trying!\n");
     }
     else {
-        printf("Invalid grade entered.\n");
+        printf("Oops, that doesn't look like a valid grade. Please try again.\n");
     }
 
     return 0;
