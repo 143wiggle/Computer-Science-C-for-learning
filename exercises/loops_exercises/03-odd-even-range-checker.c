3653ebfda72ed33ed4493f78d0a6c5cfa64215fd
bwent <belmontebrent@gmail.com>
2025-06-15 21:24:10 +0800

    03-odd-even-range-checker.c

--- exercises/loops_exercises/03-odd-even-range-checker.c
+++ exercises/loops_exercises/03-odd-even-range-checker.c
@@ -7,9 +7,15 @@
 int main() {
     int i, limit;
 
-    printf("Enter the value of N: ");
+    // Mini-purpose and instructions to user
+    printf("=== Odd or Even Checker ===\n");
+    printf("This program checks if numbers from 1 up to N are odd or even.\n");
+    printf("Enter a number (N) to define the range (1 to N): ");
+
+    // Get the range limit
     scanf("%d", &limit);
 
+    // Loop through 1 to N
     for (i = 1; i <= limit; i++) {
         if (i % 2 == 0) {
             printf("%d is even\n", i);
